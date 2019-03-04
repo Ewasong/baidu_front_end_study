7f5c527c1df49268363b2573d1c74c5211dfded4
sorie <819294006@qq.com>
2019-03-04 18:23:25 +0800

    MDN Web DOC study for css

---
+++ src/frontEnd/cssStudy/HelloWorld/scripts/main.js
@@ -0,0 +1,43 @@
+//hello world
+// var myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello World";
+
+//click demo
+// document.querySelector("html").onclick = function () {
+//     alert("Don't touch me!");
+// }
+
+//img
+// var myImage = document.querySelector("img");
+// myImage.onclick = function () {
+//     var imageSrc = myImage.getAttribute("src");
+//     if(imageSrc === "images/photo.png"){
+//         myImage.setAttribute("src", "images/noPhoto-noWord.jpg");
+//     }
+//     else {
+//         myImage.setAttribute("src", "images/photo.png");
+//
+//     }
+//
+// }
+
+//button change user
+// var myBtn = document.querySelector("button");
+// var myH1 = document.querySelector("h1");
+// function setUserName() {
+//     var myName = prompt("Pls enter you name.");
+//     localStorage.setItem("ccname", myName);
+//     myH1.textContent = "Hello, " + myName;
+// }
+// if(!localStorage.getItem("ccname")){
+//     setUserName();
+// }
+// else {
+//     var storedName = localStorage.getItem("ccname");
+//     myH1.textContent = "Hello, " + storedName;
+// }
+//
+// myBtn.onclick = function () {
+//     setUserName();
+// }
+
